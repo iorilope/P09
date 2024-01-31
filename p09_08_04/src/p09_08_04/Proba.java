7817e3c6767a8704e3ad38e8c5add95c07aed0a8
iorilope <146725975+iorilope@users.noreply.github.com>
2024-01-31 12:03:24 +0100

    P09_08 4 ariketa eginda

---
+++ p09_08_04/src/p09_08_04/Proba.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package p09_08_04;
+
+/**
+ * 
+ * @author Ioritz Lopetegi
+ */
+public class Proba {
+
+	public static void main(String args[]) {
+		
+		final String s1=new String("Kaixo ");
+		String s2= new String("Mundua ");
+		s1 = s1 + s2;
+		//f1 final denez bere balorea ezinda aldatu ebraz s1 + s2 egiterakoan errore gertztzen da 
+		
+		
+	}
+
+}

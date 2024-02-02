b992ce13884e8f93dabb231402aee89e79324bc0
iorilope <146725975+iorilope@users.noreply.github.com>
2024-02-02 12:51:09 +0100

    P09_08 6 ariketa eginda

---
+++ p09_08_06/src/p09_08_06/Ibilgailua.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package p09_08_06;
+
+/**
+ * 
+ * @author Ioritz Lopetegi
+ */
+public abstract class Ibilgailua {
+	private int pisua;
+
+	public final void setPisua(int p) {
+		pisua = p;
+	}
+
+	public abstract int getUnekoAbidura();
+}
+
+/*
+ * Bai, klase honek azpiklaseak eduki ditzazke azpiklase hauek pisua aldagai eta
+ * setpisua metodoa hartuko dute,get unkeko abidarua inplementatzeko azpiklase
+ * bat beharrrezkoa da
+ */

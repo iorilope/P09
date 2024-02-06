8a07ba2ed749a68da0518cfe1c294eb712fcb242
iorilope <146725975+iorilope@users.noreply.github.com>
2024-02-06 08:45:26 +0100

    Last

--- p09_04/src/p09_04/Arduraduna.java
+++ p09_04/src/p09_04/Arduraduna.java
@@ -1,17 +1,41 @@
+/*
+ * 2024(e)ko ots. 6(a)
+ * Ioritz Lopetegi
+ */
 package p09_04;
 
+/**
+ * The Class Arduraduna.
+ */
 public class Arduraduna extends Langilea {
+
+	/** The p. */
 	private String p;
 
+	/**
+	 * Gets the soldata.
+	 *
+	 * @return soldata
+	 */
 	public int getSoldata() {
 		Double d = Double.valueOf(soldataOinarria * 1.1);
 		return d.intValue();
 	}
 
+	/**
+	 * Sets the postua.
+	 *
+	 * @param postua new postua
+	 */
 	public void setPostua(String postua) {
 		p = postua;
 	}
 
+	/**
+	 * Gets the postua.
+	 *
+	 * @return postua
+	 */
 	public String getpostua() {
 
 		return p;

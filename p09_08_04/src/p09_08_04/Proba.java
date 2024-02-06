e60bc89d1787c3eb68b4f9e337a52d7d10dcd59b
iorilope <146725975+iorilope@users.noreply.github.com>
2024-02-06 08:57:03 +0100

    Last

--- p09_08_04/src/p09_08_04/Proba.java
+++ p09_08_04/src/p09_08_04/Proba.java
@@ -1,5 +1,6 @@
-/**
- * 
+/*
+ * 2024(e)ko ots. 6(a)
+ * Ioritz Lopetegi
  */
 package p09_08_04;
 
@@ -9,14 +10,14 @@ package p09_08_04;
  */
 public class Proba {
 
-	
-		
-		final String s1=new String("Kaixo ");
-		String s2= new String("Mundua ");
-		s1 = s1 + s2;
-		//f1 final denez bere balorea ezinda aldatu ebraz s1 + s2 egiterakoan errore gertztzen da 
-		
-		
-	}
+	/** The s 1. */
+	final String s1 = new String("Kaixo ");
+
+	/** The s 2. */
+	String s2= new String("Mundua ");s1=s1+s2;
+	// f1 final denez bere balorea ezinda aldatu ebraz s1 + s2 egiterakoan errore
+	// gertatzen da
+
+}
 
 }

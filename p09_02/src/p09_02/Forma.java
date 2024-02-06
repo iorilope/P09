b61d62fb745fc26e379660d13d5eeace77597f2b
iorilope <146725975+iorilope@users.noreply.github.com>
2024-02-06 08:42:41 +0100

    Last

--- p09_02/src/p09_02/Forma.java
+++ p09_02/src/p09_02/Forma.java
@@ -1,24 +1,37 @@
+/*
+ * 2024(e)ko ots. 6(a)
+ * Ioritz Lopetegi
+ */
 package p09_02;
 
-public abstract class Forma
-{
-	public void identitatea() 
-	{
+
+/**
+ * The Class Forma.
+ */
+public abstract class Forma {
+	
+	/**
+	 * Identitatea.
+	 */
+	public void identitatea() {
 		System.out.println(this);
 	}
 
+	/**
+	 * Inprimatu.
+	 *
+	 * @return the string
+	 */
 	public abstract String inprimatu();
 }
 
-public class Zirkulu extends Forma
-{
+public class Zirkulu extends Forma {
 	public String inprimatu() {
 		return "Zirkulua";
 	}
 }
 
-public class Karratu extends Forma
-{
+public class Karratu extends Forma {
 	public String inprimatu() {
 		return "Karratua";
 	}
--- p09_02/src/p09_02/TestForma.java
+++ p09_02/src/p09_02/TestForma.java
@@ -1,21 +1,19 @@
 package p09_02;
 
-public class TestForma
-{
-	public static void main(String args[]) 
-	{
+public class TestForma {
+	public static void main(String args[]) {
 		String forma1, forma2;
 		// Forma f0 = new Forma(); Errorea: klase abstraktuak ezin dira instantziatu
-		
+
 		Forma f1 = new Zirkulu();
 		f1.identitatea();
-		
+
 		Forma f2 = new Karratu();
 		f2.identitatea();
-		
+
 		forma1 = f1.inprimatu();
 		forma2 = f2.inprimatu();
-		
+
 		System.out.println("1. forma " + forma1 + " da eta 2. forma " + forma2);
 	}
 }

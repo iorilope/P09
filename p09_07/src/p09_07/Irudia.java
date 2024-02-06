a2dc3f7eae8bb0a29a452015e0da48ee4f4e2a7d
iorilope <146725975+iorilope@users.noreply.github.com>
2024-02-06 08:51:01 +0100

    Last

--- p09_07/src/p09_07/Irudia.java
+++ p09_07/src/p09_07/Irudia.java
@@ -1,5 +1,6 @@
-/**
- * 
+/*
+ * 2024(e)ko ots. 6(a)
+ * Ioritz Lopetegi
  */
 package p09_07;
 
@@ -8,12 +9,24 @@ package p09_07;
  * @author Ioritz Lopetegi
  */
 public class Irudia {
+
+	/** The kolorea. */
 	private String kolorea;
 
+	/**
+	 * Sets the kolorea.
+	 *
+	 * @param s the new kolorea
+	 */
 	public void setKolorea(String s) {
 		kolorea = s;
 	}
 
+	/**
+	 * Gets the kolorea.
+	 *
+	 * @return the kolorea
+	 */
 	public String getKolorea() {
 		return kolorea;
 	}
